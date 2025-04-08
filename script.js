f6355ef798fd6c8913de3238d192371475598d8d
alex meza <alex.meza.5471@gmail.com>
2025-04-08 12:19:28 -0600

    Creando nuevos elementos html desde js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
+
 let btnMostrar = document.getElementById("btnMostrar");
 
 let encabezado1 = document.getElementById("encabezado1");
 
 let encabezado2 = document.getElementById("encabezado2");
 
-let lista = document.getElementsByTagName("ul");
+let listas = document.getElementsByTagName("ul");
 
 let elementos = document.getElementsByClassName("list-group-item p-4 active")
 
@@ -12,9 +14,9 @@ let otroElemento = document.querySelector("ul>li");
 
 let otrosElementos = document.querySelectorAll("ul>li");
 
-console.log(otroElemento);
-console.log(otrosElementos);
-console.log(otrosElementos.length);
+//console.log(otroElemento);
+//console.log(otrosElementos);
+//console.log(otrosElementos.length);
 //console.log(elementos.length);
 //console.log(lista.length);
 //console.log(lista.item(0));
@@ -25,6 +27,7 @@ console.log(otrosElementos.length);
 //FUNCIONES...
 
 let contador = 0;
+
 function myFunction(){
     encabezado1.innerHTML = encabezado1.innerHTML + "<em>Hola jejej</em>"
     contador++;
@@ -33,8 +36,18 @@ function myFunction(){
 
 btnMostrar.addEventListener("click",function(event) {
     event.preventDefault();
-    alert("hola");
-     contador++;
+    let element = document.createElement("li");
+    element.innerText = "Another item";
+    element.classList.add("list-group-item");
+    element.classList.add("p-4");
+
+    let element2 = element.cloneNode(element);
+    //listas.item(0).before(element);
+    //listas.item(0).prepend(element);
+    //listas.item(0).before(element2);
+    listas.item(0).after(element);
+    listas.item(0).append(element2);
+    
     
     
 });
