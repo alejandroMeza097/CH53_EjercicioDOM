45c1807291ee3f59db616f455f7dc8478ce7cebf
alex meza <alex.meza.5471@gmail.com>
2025-04-10 09:39:19 -0600

    actualizaciones finales

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ let otrosElementos = document.querySelectorAll("ul>li");
 //console.log(lista.item(0));
 //console.log(elementos[0]);
 
+//VARIABLES PARA FORMULARIO...
+let telefono = document.getElementById("telefono");
+let curp = document.getElementById("curp");
+let rfc = document.getElementById("rfc");
+
 
 
 //FUNCIONES...
@@ -45,12 +50,56 @@ btnMostrar.addEventListener("click",function(event) {
     //listas.item(0).before(element);
     //listas.item(0).prepend(element);
     //listas.item(0).before(element2);
-    listas.item(0).after(element);
-    listas.item(0).append(element2);
-    
-    
+    //listas.item(0).after(element);
+    //listas.item(0).append(element2);
+
+    //listas.item(1).insertAdjacentElement("afterbegin",element);
+    //listas.item(1).insertAdjacentElement("beforeend",element2);
+
+    listas.item(1).insertAdjacentHTML("afterbegin",`
+        <li class="list-group-item p-4">Another element </li>
+        `);
+
+    listas.item(1).insertAdjacentHTML("beforeend",`
+        <li class="list-group-item p-4">Another element </li>
+        `);
+
+    listas.item(1).insertAdjacentHTML("afterend",`
+        <li class="list-group-item p-4">Another element </li>
+        `);
+
+    listas.item(1).insertAdjacentHTML("beforebegin",`
+        <li class="list-group-item p-4">Another element </li>
+        `);
     
 });
+
+
+window.addEventListener("load",function(event){
+    console.log("pagina cargada");
+});
+
+
+
+function txtToUpper(event){
+    event.preventDefault();
+    event.target.value = event.target.value.trim().toUpperCase();
+}
+
+
+
+curp.addEventListener("blur",txtToUpper);
+
+
+rfc.addEventListener("blur",txtToUpper);
+
+
+telefono.addEventListener("blur",function(event){
+    telefono.value = telefono.value.trim().slice(0,10);
+});
+
+
+
 
 
 
